26361ff691e9f31ba5d8a6c7f8e3af34f14d1ee4
jonwal <jon.walstedt@kambi.com>
2015-05-12 12:26:23 +0200

    added a banner to the dist files - updated coffee-task

--- dist/backbone.vigor.componentmanager.js
+++ dist/backbone.vigor.componentmanager.js
@@ -1,3 +1,9 @@
+/**
+ * backbone.vigorjs.componentmanager - Helps you decouple Backbone applications
+ * @version v0.0.1
+ * @link 
+ * @license MIT
+ */
 (function() {
   var extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
     hasProp = {}.hasOwnProperty;
